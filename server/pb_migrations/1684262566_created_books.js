2b2328675d8b11fc932b29c0e7339cb280b9cc84
maciejfec2i <Maciej.Fec@2itesting.com>
2023-05-16 19:43:10 +0100

    add book collection

---
+++ server/pb_migrations/1684262566_created_books.js
@@ -0,0 +1,103 @@
+migrate((db) => {
+  const collection = new Collection({
+    "id": "pdsesagbyf831ff",
+    "created": "2023-05-16 18:42:46.280Z",
+    "updated": "2023-05-16 18:42:46.280Z",
+    "name": "books",
+    "type": "base",
+    "system": false,
+    "schema": [
+      {
+        "system": false,
+        "id": "6leukpcy",
+        "name": "title",
+        "type": "text",
+        "required": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "69wmurkg",
+        "name": "author",
+        "type": "text",
+        "required": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "nyuwlddl",
+        "name": "description",
+        "type": "text",
+        "required": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "f9ctblkm",
+        "name": "genre",
+        "type": "text",
+        "required": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "lcv5qkvc",
+        "name": "original_publish_date",
+        "type": "date",
+        "required": false,
+        "unique": false,
+        "options": {
+          "min": "",
+          "max": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "fgdtjs6c",
+        "name": "publisher",
+        "type": "text",
+        "required": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      }
+    ],
+    "indexes": [],
+    "listRule": null,
+    "viewRule": null,
+    "createRule": null,
+    "updateRule": null,
+    "deleteRule": null,
+    "options": {}
+  });
+
+  return Dao(db).saveCollection(collection);
+}, (db) => {
+  const dao = new Dao(db);
+  const collection = dao.findCollectionByNameOrId("pdsesagbyf831ff");
+
+  return dao.deleteCollection(collection);
+})
