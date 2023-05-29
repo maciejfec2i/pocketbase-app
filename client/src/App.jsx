525f6e24f332e38acd4ce19eecb246f9a1250b17
maciejfec2i <Maciej.Fec@2itesting.com>
2023-05-29 12:28:54 +0100

    add custom components and imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
+import './App.css'
+import Body from './components/Body'
+import Header from './components/Header'
+import Navbar from './components/navbar/Navbar'
+import useGetBooks from "./hooks/useGetBooks"
+import useSelectedTab from './hooks/useSelectedTab'
+import useCurrentSort from './hooks/userCurrentSort'
 
 function App() {
 
+  const [tabs, setTabs] = useSelectedTab()
+  const [sort, setSort] = useCurrentSort()
+  const books = useGetBooks({sort})
+
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className='main-container'>
+      <Header />
+      <Navbar tabs={tabs} setSort={setSort} />
+      <Body books={books} />
+    </div>
   )
 }
 
