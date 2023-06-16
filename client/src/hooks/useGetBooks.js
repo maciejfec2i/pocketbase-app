bacbf6291da164ccb10367d9cf1d561d51ff3dca
maciejfec2i <Maciej.Fec@2itesting.com>
2023-06-16 19:07:34 +0100

    add logic to subscribe to book collection for real time updates

--- client/src/hooks/useGetBooks.js
+++ client/src/hooks/useGetBooks.js
@@ -1,16 +1,34 @@
 import { useEffect, useState } from "react";
-import { getAll } from "../utils/pb";
+import { getAll, pb } from "../utils/pb";
 
 export default function useGetBooks({sort}) {
     const [books, setBooks] = useState([])
+    const collection = "books"
+
+    let unsubscribe
 
     useEffect(() => {
         async function getBooks() {
-            const collection = "books"
             const books = await getAll({collection, sortBy: sort})
             setBooks([...books])
         }
+
+        async function subscribe() {
+            unsubscribe = await pb.collection(collection).subscribe("*", async ({action}) => {
+                if(action === "create" || action === "delete") {
+                    const allBooks = await getAll({collection, sortBy: sort})
+                    setBooks([...allBooks])
+                }
+            })
+        }
+
         getBooks()
+        
+        subscribe()
+        return () => {
+            unsubscribe?.("*")
+            console.log("Unsubbed")
+        }
     }, [sort])
 
     return books
