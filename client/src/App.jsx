6033e00bdc6e39eecbfce6f184ebd35a258642e6
maciejfec2i <Maciej.Fec@2itesting.com>
2023-06-16 20:43:18 +0100

    replace useGteBooks hook with useCollection hook

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,17 @@ import Body from './components/Body'
 import Header from './components/Header'
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/Footer'
-import useGetBooks from "./hooks/useGetBooks"
 import useNewBookForm from './hooks/useNewBookForm'
 import useCurrentSort from './hooks/userCurrentSort'
+import useCollection from './hooks/useCollection'
+import { BOOK_COLLECTION } from './utils/collections'
 
 function App() {
 
   const [sort, setSort] = useCurrentSort()
-  const books = useGetBooks({sort})
+  const books = useCollection({ collection: BOOK_COLLECTION, sortBy: sort, subscribeToCollection: true })
   const [formValues, setFormValues] = useNewBookForm()
-  console.log("hello there!")
+  console.log(books)
 
   return (
     <div className='main-container'>
