4969786db7579c6bb01926db347f4246acda0c0a
maciejfec2i <Maciej.Fec@2itesting.com>
2023-06-17 20:38:50 +0100

    add pagination

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,22 +5,24 @@ import Navbar from './components/navbar/Navbar'
 import Footer from './components/Footer'
 import useNewBookForm from './hooks/useNewBookForm'
 import useCurrentSort from './hooks/userCurrentSort'
-import useCollection from './hooks/useCollection'
+import usePaginatedCollection from './hooks/usePaginatedCollection'
 import { BOOK_COLLECTION } from './utils/collections'
+import { useState } from 'react'
 
 function App() {
 
   const [sort, setSort] = useCurrentSort()
-  const books = useCollection({ collection: BOOK_COLLECTION, sortBy: sort, subscribeToCollection: true })
+  const [currentPage, setCurrentPage] = useState(1)
+  const [books, totalPages] = usePaginatedCollection({ collection: BOOK_COLLECTION, sortBy: sort, subscribeToCollection: true, page: currentPage, itemsPerPage: 10 })
   const [formValues, setFormValues] = useNewBookForm()
   console.log(books)
 
   return (
-    <div className='main-container'>
+    <div className='main-container flex-container flex-direction-column'>
       <Header />
       <Navbar setSort={setSort} formValues={formValues} setFormValues={setFormValues} />
       <Body books={books} />
-      <Footer />
+      <Footer currentPage={currentPage} setCurrentPage={setCurrentPage} totalPages={totalPages} />
     </div>
   )
 }
