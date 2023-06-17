8b1cb00f4c7a10db8627c5002a025d62f945c830
maciejfec2i <Maciej.Fec@2itesting.com>
2023-06-17 20:40:39 +0100

    remove unnecessary nesting

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -9,19 +9,17 @@ export default function Navbar(props) {
 
   return (
     <div className="navbar flex-container centre-horizontal">
-        <div className="sort-select-container flex-container">
-            <select className="sort-select" defaultValue={"Most Relevant"} onChange={(e) => props.setSort(e.target.value)}>
-                <option vlaue="Most Relevant">Most Relevant</option>
-                <option value="Title (A-Z)">Title (A - Z)</option>
-                <option value="Title (Z-A)">Title (Z - A)</option>
-                <option value="Author (A-Z)">Author (A - Z)</option>
-                <option value="Author (Z-A)">Author (Z - A)</option>
-                <option value="Relase Date (Ascending)">Relase Date (Ascending)</option>
-                <option value="Relase Date (Descending)">Relase Date (Descending)</option>
-            </select>
-            <button className="add-book-btn" onClick={() => {setShow(true)}}>Add Book</button>
-            <Modal show={show} setShow={setShow} modalTitle={"Add New Book"} modalBody={<NewBookForm formValues={props.formValues} setFormValues={props.setFormValues} setShow={setShow} />} />
-        </div>
+      <select className="sort-select navbar-item" defaultValue={"Most Relevant"} onChange={(e) => props.setSort(e.target.value)}>
+        <option vlaue="Most Relevant">Most Relevant</option>
+        <option value="Title (A-Z)">Title (A - Z)</option>
+        <option value="Title (Z-A)">Title (Z - A)</option>
+        <option value="Author (A-Z)">Author (A - Z)</option>
+        <option value="Author (Z-A)">Author (Z - A)</option>
+        <option value="Relase Date (Ascending)">Relase Date (Ascending)</option>
+        <option value="Relase Date (Descending)">Relase Date (Descending)</option>
+      </select>
+      <button id="add-book-btn" className="btn-main-dark navbar-item" onClick={() => {setShow(true)}}>Add Book</button>
+      <Modal show={show} setShow={setShow} modalTitle={"Add New Book"} modalBody={<NewBookForm formValues={props.formValues} setFormValues={props.setFormValues} setShow={setShow} />} />
     </div>
   )
 }
