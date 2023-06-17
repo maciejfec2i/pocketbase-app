ac8d6423a181204df57271610b35021a8fc15d84
maciejfec2i <Maciej.Fec@2itesting.com>
2023-06-17 20:39:47 +0100

    add footer content

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,25 @@
-export default function Footer() {
+import { changePage } from "../utils/utilities";
+
+export default function Footer(props) {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div>
+        <div className="pagination flex-container pagination centre">
+          <button className="btn-main-dark" 
+          disabled={props.currentPage === 1 ? true: false}
+          onClick={() => changePage({ currentPage: props.currentPage, setCurrentPage: props.setCurrentPage, direction: "prev" })}>
+            Prev
+          </button>
+          
+          <p className="pages text-not-selectable">{props.currentPage} of {props.totalPages}</p>
+          
+          <button className="btn-main-dark" 
+          disabled={props.currentPage === props.totalPages ? true : false} 
+          onClick={() => changePage({ currentPage: props.currentPage, setCurrentPage: props.setCurrentPage, direction: "next" })}>
+            Next
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
