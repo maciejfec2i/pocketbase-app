1751766091a269577cdf0fc1d97eda3c304e5d67
maciejfec2i <Maciej.Fec@2itesting.com>
2023-06-18 15:56:55 +0100

    update title

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,9 +3,9 @@ import bookIcon from "../assets/books.png"
 export default function Header() {
   return (
     <div className="header flex-container centre-text centre-horizontal">
-      <img className="logo" src={bookIcon} alt="stack og books icon" />
+      <img className="logo" src={bookIcon} alt="stack of books icon" />
       <div className="title text-not-selectable">
-        <h1>Share Your Book</h1>
+        <h1>Book Repository</h1>
       </div>
     </div>
   )
