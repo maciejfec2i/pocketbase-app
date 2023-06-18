7c85666d1ad6f157ce9872498333d30f0d855a47
maciejfec2i <Maciej.Fec@2itesting.com>
2023-06-18 15:28:03 +0100

    update custom hook

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,22 +7,24 @@ import useNewBookForm from './hooks/useNewBookForm'
 import useCurrentSort from './hooks/userCurrentSort'
 import usePaginatedCollection from './hooks/usePaginatedCollection'
 import { BOOK_COLLECTION } from './utils/collections'
-import { useState } from 'react'
 
 function App() {
 
   const [sort, setSort] = useCurrentSort()
-  const [currentPage, setCurrentPage] = useState(1)
-  const [books, totalPages] = usePaginatedCollection({ collection: BOOK_COLLECTION, sortBy: sort, subscribeToCollection: true, page: currentPage, itemsPerPage: 10 })
+  const [books, totalPages, currentPage, setCurrentPage, currentPageRef] = usePaginatedCollection({ 
+    collection: BOOK_COLLECTION,
+    sortBy: sort,
+    subscribeToCollection: true,
+    itemsPerPage: 10
+  })
   const [formValues, setFormValues] = useNewBookForm()
-  console.log(books)
 
   return (
     <div className='main-container flex-container flex-direction-column'>
       <Header />
       <Navbar setSort={setSort} formValues={formValues} setFormValues={setFormValues} />
       <Body books={books} />
-      <Footer currentPage={currentPage} setCurrentPage={setCurrentPage} totalPages={totalPages} />
+      <Footer currentPage={currentPage} setCurrentPage={setCurrentPage} totalPages={totalPages} currentPageRef={currentPageRef} />
     </div>
   )
 }
