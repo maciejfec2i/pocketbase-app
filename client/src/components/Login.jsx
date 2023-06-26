88d0ec8b6244bdfa7ed2d88e86381035106a4218
maciejfec2i <Maciej.Fec@2itesting.com>
2023-06-26 20:07:08 +0100

    add html

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import LoginForm from './LoginForm'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className="main-container flex-container centre">
+      <div className="flex-container centre login-page-inner">
+        <LoginForm />
+      </div>
+    </div>
   )
 }
