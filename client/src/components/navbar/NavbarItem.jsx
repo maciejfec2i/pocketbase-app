4cbb6825d7dcf2f3323139f67007871239bce2cb
maciejfec2i <Maciej.Fec@2itesting.com>
2023-06-27 13:07:54 +0100

    deleted as no longer needed

--- client/src/components/navbar/NavbarItem.jsx
+++
@@ -1,5 +0,0 @@
-export default function NavbarItem({ tab }) {
-  return (
-    <li className="navbar-item"><span className={tab.selected ? "selected" : ""}>{tab.tabName}</span></li>
-  )
-}
--- client/src/components/navbar/NavbarItems.jsx
+++
@@ -1,9 +0,0 @@
-import NavbarItem from "./NavbarItem"
-
-export default function NavbarItems({ tabs }) {
-  return (
-    tabs.map(tab => {
-        return <NavbarItem key={crypto.randomUUID()} tab={tab} />
-    })
-  )
-}
