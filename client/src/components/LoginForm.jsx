5485fb0595f0f7ded4177056ab258f0244e61d15
maciejfec2i <Maciej.Fec@2itesting.com>
2023-06-27 13:11:35 +0100

    add logic for keeping track of form inputs

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { authUser } from '../utils/pb'
+import { handleFormInputChange } from '../utils/utilities'
 
-export default function LoginForm() {
+export default function LoginForm(props) {
+
+  const [credentials, setCredentials] = useState({email: "", password: ""})
+
   return (
-    <form className="form">
-        <input className="form-txt-input" type="email" id="email" name="email" placeholder="Email" />
-        <input className="form-txt-input" type="password" id="password" name="password" placeholder="Password" />
+    <form className="form" onSubmit={e => authUser({ event: e, email: credentials.email, password: credentials.password, setLoggedIn: props.setLoggedIn })}>
+        <input className="form-txt-input" type="email" id="email" name="email" placeholder="Email" onChange={e => handleFormInputChange({ e, formValues: credentials, setFormValues: setCredentials })} />
+        <input className="form-txt-input" type="password" id="password" name="password" placeholder="Password" onChange={e => handleFormInputChange({ e, formValues: credentials, setFormValues: setCredentials })} />
         <input className="form-submit-btn" type="submit" id="login-btn" value={"Login"} />
     </form>
   )
