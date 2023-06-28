47277de0b1c3097e0a096ec214db5f3eb6f605cd
maciejfec2i <Maciej.Fec@2itesting.com>
2023-06-28 09:40:26 +0100

    add logic for clicking login button

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,15 +1,39 @@
-import React, { useState } from 'react'
-import { authUser } from '../utils/pb'
+import { useState } from 'react'
 import { handleFormInputChange } from '../utils/utilities'
+import { authenticator } from '../utils/pb'
+import { useNavigate } from 'react-router-dom'
 
 export default function LoginForm(props) {
 
   const [credentials, setCredentials] = useState({email: "", password: ""})
+  const [error, setError] = useState()
+  const navigateTo = useNavigate()
+
+  const HOME_PAGE_URL = "/"
 
   return (
-    <form className="form" onSubmit={e => authUser({ event: e, email: credentials.email, password: credentials.password, setLoggedIn: props.setLoggedIn })}>
+    <form className="form" onSubmit={
+      async (e) => {
+        e.preventDefault()
+        setError(false)
+        const result = await authenticator.withCredentials({
+          usernameOrEmail: credentials.email,
+          password: credentials.password
+        })
+        .authenticateAs("user")
+        
+        if(result?.error) setError(result.error)
+        else {
+          props.setLoggedIn(authenticator.authStoreIsValid())
+          navigateTo(HOME_PAGE_URL)
+        }
+      }
+    }>
         <input className="form-txt-input" type="email" id="email" name="email" placeholder="Email" onChange={e => handleFormInputChange({ e, formValues: credentials, setFormValues: setCredentials })} />
         <input className="form-txt-input" type="password" id="password" name="password" placeholder="Password" onChange={e => handleFormInputChange({ e, formValues: credentials, setFormValues: setCredentials })} />
+        {error && 
+          <p>{error}</p>
+        }
         <input className="form-submit-btn" type="submit" id="login-btn" value={"Login"} />
     </form>
   )
