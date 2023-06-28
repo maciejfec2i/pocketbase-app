4f60513399662d3fc9db40aca85adf1a5f2ed764
maciejfec2i <Maciej.Fec@2itesting.com>
2023-06-28 09:38:15 +0100

    add class for authentication

--- client/src/utils/pb.js
+++ client/src/utils/pb.js
@@ -1,50 +1,51 @@
 import PocketBase from "pocketbase"
-import { selectSortBy } from "./utilities"
 
-class PocketBaseClient {
+class PbRequestBuilder {
 
-    #pocketBaseInstance
-    #collectionName
-    #queryParams
-    #pageNumber
+    _collectionName = ""
+    _queryParams = {}
+    _pageNumber = 1
     
-    constructor(pocketBaseInstance) {
-        this.#pocketBaseInstance = pocketBaseInstance
-        this.#collectionName = ""
-        this.#queryParams = {}
-        this.#pageNumber = 1
-    }
-
     fromCollectionNamed(collectionName) {
-        this.#collectionName = collectionName
+        this._collectionName = collectionName
         return this
     }
 
     inCollectionNamed(collectionName) {
-        this.#collectionName = collectionName
+        this._collectionName = collectionName
         return this
     }
 
     withQueryParams(queryParams) {
-        this.#queryParams = queryParams
+        this._queryParams = queryParams
         return this
     }
 
     fromPage(pageNumber) {
-        this.#pageNumber = pageNumber
+        this._pageNumber = pageNumber
         return this
+    }
+}
+
+class PocketBaseClient extends PbRequestBuilder {
+
+    #pocketBaseInstance
+    
+    constructor(pocketBaseInstance) {
+        super()
+        this.#pocketBaseInstance = pocketBaseInstance
     }
 
     async getAllRecords() {
         return await this.#pocketBaseInstance
-            .collection(this.#collectionName)
-            .getFullList(this.#queryParams)
+            .collection(this._collectionName)
+            .getFullList(this._queryParams)
     }
 
     async getRecords({ perPage }) {
         const paginatedCollection = await this.#pocketBaseInstance
-            .collection(this.#collectionName)
-            .getList(this.#pageNumber, perPage, this.#queryParams)
+            .collection(this._collectionName)
+            .getList(this._pageNumber, perPage, this._queryParams)
 
         let totalPages = Math.ceil(paginatedCollection.totalItems / perPage)
         totalPages = totalPages === 0 ? totalPages += 1 : totalPages
@@ -54,28 +55,68 @@ class PocketBaseClient {
 
     async createRecord(record) {
         return await this.#pocketBaseInstance
-            .collection(this.#collectionName)
+            .collection(this._collectionName)
             .create(record)
     }
 
     async listenForChanges(topic, callback) {
-        return pb.collection(this.#collectionName)
+        return this.#pocketBaseInstance.collection(this._collectionName)
             .subscribe(topic, callback)
     }
 }
 
-const pb = new PocketBase("http://127.0.0.1:8090")
-pb.autoCancellation(false)
+class PocketBaseClientAuthenticator {
 
-async function authUser({ event, email, password, setLoggedIn }) {
-    event.preventDefault()
-    const authData = await pb.collection("users").authWithPassword(email, password)
-    console.log(authData)
+    #backendClient
+    #usernameOrEmail
+    #password
+
+    constructor(backendClient) {
+        this.#backendClient = backendClient
+        this.#usernameOrEmail = ""
+        this.#password = ""
+    }
+
+    withCredentials({ usernameOrEmail, password }) {
+        this.#usernameOrEmail = usernameOrEmail
+        this.#password = password
+
+        return this
+    }
+
+    async authenticateAs(userType) {
+        try {
+            switch(userType.toLowerCase()) {
+                case "admin":
+                    await this.#backendClient.admins.authWithPassword(this.#usernameOrEmail, this.#password)
+                    break
+                case "user":
+                    await this.#backendClient.collection("users").authWithPassword(this.#usernameOrEmail, this.#password)
+                    break
+            }
+        }
+        catch(err) {
+            console.log(err.data.message)
+            return { error: err.data.message }
+        }
+    }
+
+    clearAuthStore() {
+        this.#backendClient.authStore.clear()
+    }
+
+    authStoreIsValid() {
+        return this.#backendClient.authStore.isValid
+    }
 }
 
-const backendClient = new PocketBaseClient(pb)
+const POCKET_BASE_INSTANCE = new PocketBase("http://127.0.0.1:8090")
+POCKET_BASE_INSTANCE.autoCancellation(false)
+
+const backendClient = new PocketBaseClient(POCKET_BASE_INSTANCE)
+const authenticator = new PocketBaseClientAuthenticator(POCKET_BASE_INSTANCE)
 
 export {
-    authUser,
-    backendClient
+    backendClient,
+    authenticator
 }
